898c473ce7fe88698a6d0197f696160ec16dc8e9
Dhanuka99 <danuka1999lakshan@gmail.com>
2021-06-26 22:51:16 +0530

    first commit

---
+++ first.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+        <?php
+
+echo "<h2>Hello Fist PHP</h2>";
+echo "Hello world using echo </br>";  
+ECHO "Hello world using ECHO </br>";  
+EcHo "Hello world using EcHo </br>";  
+
+$color = "Black";
+$name = "Dhanuka";
+echo "My car is $color color. My name is $name </br>";
+
+echo $color,$name;
+
+echo "Hello by PHP echo  
+this is multi line  
+text printed by   
+PHP echo statement  
+</br>";
+
+echo "Hello escape \"sequence\" charactor</br>";
+
+$msg = "Hello php. this is important message";
+
+echo "Message : $msg";
+$arg = "SomeOne";
+
+print($arg);
+
+
+
+        ?>
+    </body>
+</html>
